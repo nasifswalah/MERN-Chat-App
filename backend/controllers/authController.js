a3859c26418781b635ae412bc38556af9feefe27
nasifswalah <nasifswalah@gmail.com>
2024-04-18 00:17:12 +0530

    setup the login endpoint

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -52,9 +52,29 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login = (req, res) => {
-    res.send("Login Page");
-    console.log("Login User");
+export const login = async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if (!user || !isPasswordCorrect) {
+           return res.status(400).json({error: "Invalid username or password"});
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 };
 
 export const logout = (req, res) => {
