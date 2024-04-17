ccf3d81e58896e1e20994035456972c6951cacbb
nasifswalah <nasifswalah@gmail.com>
2024-04-17 12:40:59 +0530

    setup the dependencies for auth routes

---
+++ backend/controllers/authController.js
@@ -0,0 +1,14 @@
+export const signup = (req, res) => {
+    res.send("Signup Page");
+    console.log("Signup User");
+};
+
+export const login = (req, res) => {
+    res.send("Login Page");
+    console.log("Login User");
+};
+
+export const logout = (req, res) => {
+    res.send("Logout Page");
+    console.log("Logout User");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js'
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
-app.get("/", (req, res)=>{
-    res.send("Hello");
-})
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT, ()=> console.log(`Server running at ${PORT}`))
+app.listen(PORT, ()=> console.log(`Server running at ${PORT}`));
